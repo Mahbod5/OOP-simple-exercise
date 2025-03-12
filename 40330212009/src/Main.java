790fe76ebd94e81ed844d77dd866588bef5684dd
Mahbod5 <abzmahbod@gmail.com>
2025-03-12 14:09:13 +0330

    finished

---
+++ 40330212009/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Person number1= new Person("Mahbod", 19);
+        Person number2= new Person("Mahan", 16);
+        number1.display();
+        number2.display();
+        number1.adult();
+    }
+}
